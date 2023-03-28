6f446d0ae765890352935604fdf86c470bb1fb89
jesuscmdev <g.zuscm@gmail.com>
2023-03-27 21:39:22 -0600

    Avance

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const path = require("path");
+module.exports = {
+  trailingSlash: false,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "./src/styles")],
+  },
+};
